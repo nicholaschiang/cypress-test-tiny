217a571967a94c761aca8d703d6d13c0fa1290a5
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2020-09-28 15:06:36 -0700

    feat(integration/spec.js): add failing attr assertions
    Demonstrates the bug described in cypress-io/cypress#8552 by chaining
    some debug assertions unto a `should('have.attr', 'id')` call.

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,6 +1,12 @@
 /// <reference types="cypress" />
 describe('page', () => {
-  it('works', () => {
+  it('fails to properly chain attr assertions', () => {
     cy.visit('https://example.cypress.io')
+
+    cy.get('#utilities').should('have.attr', 'id').then((attr) => {
+      console.log('This is a string, not an HTMLElement:', attr)
+      expect(attr).not.to.equal('utilities')
+      expect(attr).not.to.satisfy((attr) => typeof attr === 'string')
+    })
   })
 })
